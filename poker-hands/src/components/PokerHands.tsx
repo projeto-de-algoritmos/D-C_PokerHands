48874120c9b57b08545671bb1b5d461be75315a2
enggabrielalbino@gmail.com <enggabrielalbino@gmail.com>
2022-03-27 20:34:11 -0300

    Adicionando estilização

--- poker-hands/src/components/PokerHands.tsx
+++ poker-hands/src/components/PokerHands.tsx
@@ -2,29 +2,34 @@ import Card from '../models/card';
 import Deck from '../models/Deck';
 import Poker from '../util/Poker';
 import maxSubArraySum from '../util/maxSubArraySum';
+import "./PokerHands.scss";
 const PokerHands = () => {
   const deck = new Deck();
-  const nHands = 5;
+  const nHands = 100;
   let pokerHands : Array <Array<Card>> = [];
   for (let i = 0; i < nHands; i++) {
     pokerHands.push(deck.drawHand());   
   }
   const cardMap = (card: Card, index: number) => (
-    <li key={index}>{card ? card.card + card.suit : 'Empty'}</li>
+    <li key={index}><img src={card ? 'images/' + card.card + card.suit + '.png' : 'Empty'}/></li>
   );
   const handMap = (hand: Card[], index: number) => (
         <div key={index}>
-          <h3>Hand {index + 1} (value: {Poker.ValueHand(hand).value}) </h3>
+          <p><b>{Poker.ValueHand(hand).name}</b></p>
           <ul>
             {hand.map(cardMap)}
           </ul>
         </div>
       )
   return (
-    <div>
-      {pokerHands.map(handMap)}
-      <h3>Max Sub Array Sum</h3>
-      <p>{maxSubArraySum(pokerHands).hands.map(handMap)}</p>
+    <div className="hands">
+      <div className="d-flex">
+        {pokerHands.map(handMap)}
+      </div>
+      <h3>Melhor sequência de mãos</h3>
+      <div className="d-flex justify-content-center">
+        {maxSubArraySum(pokerHands).hands.map(handMap)}
+      </div>
     </div>
   );
 }
